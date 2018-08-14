99f0ca3a51ab1f355e070069943cb365b9dea5fb
Олег Лобаков <oleg.lobakov@gmail.com>
2018-08-14 16:08:01 +0300

    Update

--- WarehouseControlSystem/WarehouseControlSystem/ViewModel/Base/NAVEntryBaseViewModel.cs
+++ WarehouseControlSystem/WarehouseControlSystem/ViewModel/Base/NAVEntryBaseViewModel.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿// ----------------------------------------------------------------------------------
+// Copyright © 2018, Oleg Lobakov, Contacts: <oleg.lobakov@gmail.com>
+// Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// https://github.com/OlegLobakov/WarehouseControlSystem/blob/master/LICENSE
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
